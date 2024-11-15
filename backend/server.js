40ac3aae0d5a128b0c501a94ca48716e83c43d60
brandonmartin9785 <brandonmartin9785@gmail.com>
2024-11-14 21:58:05 -0500

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const searchRoutes = require('./routes/search');
 const historyRoutes = require('./routes/history');
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/search', searchRoutes);
 app.use('/api/history', historyRoutes);
